a36d8685eb21ad8e55d86ca7b8e5e685d8f8a3b4
Francisco Henrique do E. S. Oliveira <2016118050114@ifpi.int>
2018-11-11 05:32:22 -0200

    Metodo onSelect ()

--- client/src/app/heroes/heroes.component.ts
+++ client/src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HEROES } from '../mock-herores';
 import { Hero } from '../hero';
 
 @Component({
@@ -7,9 +8,10 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: "Teste"
+  heroes = HEROES;
+  selectedHero: Hero;
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
   }
   constructor() { }
 
---
+++ client/src/app/mock-herores.ts
@@ -0,0 +1,8 @@
+import { Hero } from "./hero";
+
+
+export const HEROES: Hero[] = [
+  {id: 11, name: "All migth"},
+  {id: 12, name: "Mydoria"},
+  {id: 13, name: "Todoroki"}
+];
